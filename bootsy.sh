995eb52ee7977deff1a13dadf3d23f5e8c563200
IndustryBestPractice <IndustryBestPractice2@gmail.com>
2019-09-02 07:14:11 -0400

    Updated README and wanted these instructions to follow

--- bootsy.sh
+++ bootsy.sh
@@ -80,19 +80,21 @@ fi
 usage="$(basename "$0") [-h] [-i /install/path] [-s] [-c /path/to/iplist.csv] [-w /path/to/wordlist] [-l /path/to/syslog/config]
 
 where (Note: All switches are optional and you will be prompted for those you don't specify):
-	-h  Display this help message
-	-i  Install path
-	-s  Silent switch. Don't prompt for validation of versions. See our github for example files.
-		Silent switch expects the following to exist:
-			$start_dir/ipList.csv
-			$start_dir/syslog_config
-		Silent switch does the following:
-			> Downloads rockyou for the wordlist
-			> Creates folder $start_dir if it doesn't already exist
-	-c  IPList.csv file path
-	-w  Wordlist file path (adding this option stops the download of rockyou)
-	-l  Syslog config file path (leave this option blank to load our default config)
-	-u  Runs just the security portion of the script"
+        -h  Display this help message
+        -i  Install path
+        -s  Silent switch. Don't prompt for validation of versions and other stuff. See our github for example files.
+                Silent switch expects the following to exist:
+                        $start_dir/ipList.csv
+                        $start_dir/syslog_config
+                Silent switch does the following:
+                        > Checks if funkList2000.txt exists and if not, checks for
+                          user input file, and if not exists then downloads rockyou for the wordlist
+                        > Creates folder $start_dir if it doesn't already exist
+                        > Does not prompt for email\syslog config at all
+        -c  IPList.csv file path
+        -w  Wordlist file path (adding this option stops the download of rockyou)
+        -l  Syslog config file path (leave this option blank to load our default config)
+        -u  Runs just the security portion of the script"
 
 # Adding input for a silent parameter so we don't bother the user if they want to run this quietly
 # Parameters are added using double dashes. EX) --help
